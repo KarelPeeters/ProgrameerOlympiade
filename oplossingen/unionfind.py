763d5f74bbc684a8b1393f45a70b44e0e98d8cfd
henrykvdb <henrykdev@gmail.com>
2024-03-05 22:41:04 +0100

    Update unionfind to be set based

--- oplossingen/unionfind.py
+++ oplossingen/unionfind.py
@@ -1,22 +1,30 @@
-class UnionFind:
-    def __init__(self, size):
-        # Input amount of nodes
-        self.size = size
+from collections import defaultdict
 
+
+class UnionFind:
+    def __init__(self):
         # Parent of every node
-        self.parent = [i for i in range(size)]
+        self.parent = {}
 
         # Rank of every node
         # not sure if kept very clean, mainly used for union-ing efficiently
         # would advise against using it probably?
-        self.rank = [1] * size
+        self.rank = {}
 
     # Find the parent of a node
     # This performs PARTIAL path compression to speed up further
     # find operations. Could be more or less considered constant time?
     def find(self, x):
-        if x == self.parent[x]:
+        # Create node, no parent
+        if x not in self.parent:
+            self.parent[x] = x
             return x
+
+        # Existing node, own parent
+        if x == self.parent.get(x, x):
+            return x
+
+        # Not own parent, continue search
         self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
@@ -24,13 +32,27 @@ class UnionFind:
         parentX = self.find(x)
         parentY = self.find(y)
         if parentX != parentY:
-            if self.rank[parentX] > self.rank[parentY]:
+            if self.rank.get(parentX, 1) > self.rank.get(parentY, 1):
                 self.parent[parentY] = parentX
-            elif self.rank[parentX] < self.rank[parentY]:
+            elif self.rank.get(parentX, 1) < self.rank.get(parentY, 1):
                 self.parent[parentX] = parentY
             else:
                 self.parent[parentY] = parentX
-                self.rank[parentX] += 1
+                self.rank[parentX] = self.rank.get(parentX, 1) + 1
 
+    # Returns wether this node exists in the union
+    def does_union_contain(self, x):
+        return x in self.parent
+
+    # Get a list of all connected nodes List[Set[]]
+    def get_all_unions(self):
+        children = defaultdict(list)
+        for node in self.parent.keys():
+            node_parent = self.find(node)
+            children[node_parent].append(node)
+        return children.values()
+
+    # Return the amount of connected components
+    # This _should_ equal len(self.get_all_unions())
     def calc_nbr_connected_components(self):
-        return len(set(self.find(i) for i in range(self.size)))
+        return len(set(self.find(node) for node in self.parent.keys()))
