fd96b36e51875a3937007588b5af05bdae9a7a09
Karel Peeters <karel.peeters.leuven@gmail.com>
2024-03-07 19:59:36 +0100

    Solve tevreden

---
+++ oplossingen/2024/tevreden_after.py
@@ -0,0 +1,34 @@
+from oplossingen.lib import readint, readints
+
+
+cases = readint()
+
+for case in range(cases):
+    ma, mb, n = readints()
+    pairs = [tuple(readints()) for _ in range(n)]
+
+    best = 0
+    cache = set()
+
+    def solve(left: int, taken: int, ca: int, cb: int):
+        key = (left, taken, ca, cb)
+        if key in cache:
+            return
+        cache.add(key)
+
+        global best
+        assert ca <= ma and cb <= mb
+        if left + taken < best:
+            return
+        if left == 0:
+            best = max(best, taken)
+            return
+
+        solve(left - 1, taken, ca, cb)
+
+        a, b = pairs[left - 1]
+        if ca + a <= ma and cb + b <= mb:
+            solve(left - 1, taken + 1, ca + a, cb + b)
+
+    solve(len(pairs), 0, 0, 0)
+    print(case + 1, best)
